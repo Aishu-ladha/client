a67dfb4b99d84490a00b0d907a333f89c413ec01
Aishu-ladha <100275145+Aishu-ladha@users.noreply.github.com>
2024-04-10 16:29:44 +0530

    navbar created

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -8,80 +8,39 @@ import { navBarLinks } from "../../../constants/constant";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+
+    const [menuActive, setMenuActive] = useState(false);
+
+    // Function to toggle the menu
+    const toggleMenu = () => {
+      setMenuActive(!menuActive);
+    };
   return (
     <div>
-      <header>
-        <div className="menu">
-          <RxHamburgerMenu />
-        </div>
-        <ul className="menu-options">
-          <li>
-            <a href="#">Women's Fashion</a>
-          </li>
-          <li>
-            <a href="#">Men's fashion</a>
-          </li>
-          <li>
-            <a href="#">Electronics</a>
-          </li>
-          <li>
-            <a href="#">Home & Electronics</a>
-          </li>
-        </ul>
-        <div className="logo_container">
-          <a href="#">
-            <img className="store_logo" src="/logo.jpeg" alt="myntra logo" />
-          </a>
-        </div>
+      <header className="header" id="header">
+      <nav className="navbar">
+        <div className="max-width">
+            <div className="logo"><a href="#"><img className="imglogo" src="/logo.jpeg" alt="logo" /></a></div>
+            <ul className="menu">
+                <li><a href="#" className="menu-btn">Home</a></li>
+                <li><a href="#" className="menu-btn">About</a></li>
+                <li><a href="#" className="menu-btn">contact-us</a></li>
+                <li><a href="#" className="menu-btn">Events</a></li>
+                <li><a href="#" className="menu-btn">Fan-Club</a></li>
+                <li><a href="#" className="menu-btn">Login</a></li>
+                <li><a href="#" className="menu-btn">Sign UP</a></li>
+                <li><input type="text" placeholder="Seaarch Here!!" /></li>
+                
 
-        <nav className="nav_bar">
-          {navBarLinks.map((Element) => {
-            return (
-              <div key={Element.link} className="activeHandler">
-                <NavLink
-                  className="navElements"
-                  key={Element.link}
-                  to={Element.link}
-                >
-                  {Element.name}
-                </NavLink>
-              </div>
-            );
-          })}
-        </nav>
 
-        <div className="search_bar">
-          <span className="material-symbols-outlined search_icon">
-            <FaSearch />
-          </span>
-          <input
-            className="search_input"
-            placeholder="Search for products, brands and more"
-          />
+            </ul>
+            <div className="menu-btn">
+                <i className="fas fa-bars">|</i>
+            </div>
         </div>
+    </nav>
 
-        <div>
-          <a href="#">
-            <button className="log">Login</button>
-          </a>
-        </div>
-
-        <div className="action_bar">
-          <div className="action_container">
-            <a href="#" className="material-symbols-outlined action_icon">
-              <FaRegHeart />
-            </a>
-            <span className="action_name">Wishlist</span>
-          </div>
-
-          <div className="action_container">
-            <a href="#" className="material-symbols-outlined action_icon">
-              <FiShoppingCart />
-            </a>
-            <span className="action_name">Cart</span>
-          </div>
-        </div>
-      </header>
+        </header>
       <hr />
     </div>
   );
